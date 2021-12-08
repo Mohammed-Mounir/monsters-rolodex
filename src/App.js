40f94ad5845d60f4206dbe6855e94effe1a3ba8c
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-12-08 15:08:50 +0200

    feat: SearchBox Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import './App.css';
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -14,10 +17,23 @@ class App extends Component {
     this.setState({ monsters: users });
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
